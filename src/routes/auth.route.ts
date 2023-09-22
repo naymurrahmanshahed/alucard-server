74f293a66418ca98ad7c834c1d3496e8aa719417
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-09-22 16:22:12 +0600

    routes added

---
+++ src/routes/auth.route.ts
@@ -0,0 +1,13 @@
+import express, { Router } from 'express';
+
+const authRouter: Router = express.Router();
+
+//register
+
+authRouter.post('/register');
+
+//login
+
+authRouter.post('/login');
+
+export default authRouter;
