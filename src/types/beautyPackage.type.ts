77544ce5472bb8b9ce779558c74c5972529e1c1d
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-09-22 15:15:19 +0600

    beauty package model added

--- src/types/beautyPackage.type.ts
+++ src/types/beautyPackage.type.ts
@@ -6,7 +6,7 @@ export type beautyPackageType = {
   title: string;
   description: string;
   category: string;
-  image: string[];
+  images: string[];
   price: number;
   specialists: specialistType[];
   bookings: bookingType[];
